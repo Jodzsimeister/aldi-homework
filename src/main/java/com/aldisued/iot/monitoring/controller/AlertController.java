a7e840c62d408ac642575b8ab30021205250aaa7
jtakacs <pti.jodzsi@gmail.com>
2026-08-13 17:15:14 +0200

    Task6: Implemented AlertService.saveAlert, and created HTTP post endpoint for creating alerts

--- src/main/java/com/aldisued/iot/monitoring/controller/AlertController.java
+++ src/main/java/com/aldisued/iot/monitoring/controller/AlertController.java
@@ -2,7 +2,10 @@ package com.aldisued.iot.monitoring.controller;
 
 import com.aldisued.iot.monitoring.dto.AlertDto;
 import com.aldisued.iot.monitoring.service.AlertService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,10 @@ public class AlertController {
   @GetMapping("/latest")
   public AlertDto getLatestAlerts(@RequestParam(name = "sensorId") UUID sensorId) {
     return alertService.findLastAlertBySensorId(sensorId);
+  }
+
+  @PostMapping
+  public AlertDto saveAlert(@RequestBody @Valid AlertDto alertDto) {
+    return alertService.saveAlert(alertDto);
   }
 }
--- src/main/java/com/aldisued/iot/monitoring/dto/AlertDto.java
+++ src/main/java/com/aldisued/iot/monitoring/dto/AlertDto.java
@@ -1,11 +1,14 @@
 package com.aldisued.iot.monitoring.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record AlertDto(
-    UUID sensorId,
-    String message,
-    LocalDateTime timestamp
+    @NotNull(message = "The sensor id must not be null.") UUID sensorId,
+    @NotBlank(message = "The message must not be blank.") String message,
+    @NotNull(message = "The timestamp must not be null.") LocalDateTime timestamp
 ) {
 }
