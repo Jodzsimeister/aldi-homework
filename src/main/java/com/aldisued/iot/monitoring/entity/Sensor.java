276c80e8e3a7221b49f1a6eaec6a1bfc830b50d4
jtakacs <pti.jodzsi@gmail.com>
2026-08-13 08:00:53 +0200

    Task2: Implemented required relationships betweer Sensor, SensorReading and Alert entities.

--- src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
+++ src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
@@ -1,5 +1,6 @@
 package com.aldisued.iot.monitoring.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.util.List;
@@ -27,6 +29,12 @@ public class Sensor {
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
   private SensorType type;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "sensor")
+  private List<Alert> alerts;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "sensor")
+  private List<SensorReading> sensorReadings;
 
   public Sensor() {}
 
@@ -60,22 +68,20 @@ public class Sensor {
   }
 
   public List<Alert> getAlerts() {
-    //TODO: Task 2
-    return null;
+    return this.alerts;
   }
 
   public void setAlerts(List<Alert> alerts) {
-    //TODO: Task 2
+    this.alerts = alerts;
   }
 
   public List<SensorReading> getSensorReadings() {
-    //TODO: Task 2
-    return null;
+    return this.sensorReadings;
   }
 
   public void setSensorReadings(
       List<SensorReading> sensorReadings) {
-    //TODO: Task 2
+    this.sensorReadings = sensorReadings;
   }
 
   @Override
