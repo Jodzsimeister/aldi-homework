771050fd14fcb2ef2b9038c9586a86fe377410e2
jtakacs <pti.jodzsi@gmail.com>
2026-08-13 17:54:39 +0200

    Task7: Implemented MeasurementService.getAverageTemperature

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
@@ -1,9 +1,11 @@
 package com.aldisued.iot.monitoring.service;
 
+import com.aldisued.iot.monitoring.entity.SensorReading;
 import com.aldisued.iot.monitoring.entity.SensorType;
 import com.aldisued.iot.monitoring.repository.SensorReadingRepository;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 
@@ -23,8 +25,14 @@ public class MeasurementService {
   }
 
   public Optional<Double> getAverageTemperature(LocalDateTime from, LocalDateTime to) {
-    // TODO: Task 7
-    return Optional.empty();
+    Objects.requireNonNull(from, "from must not be null");
+    Objects.requireNonNull(to, "to must not be null");
+    return sensorReadingRepository.findAllBySensorTypeAndTimestampBetween(SensorType.TEMPERATURE, from, to).stream()
+        .mapToDouble(SensorReading::getValue)
+        .average()
+        .stream()
+        .boxed()
+        .findFirst();
   }
 
 }
