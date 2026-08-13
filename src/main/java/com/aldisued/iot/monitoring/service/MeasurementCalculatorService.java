974bf2ab7929b1858a635537a0ac82b3232d27b2
jtakacs <pti.jodzsi@gmail.com>
2026-08-13 18:27:21 +0200

    Task9: Implemented MeasurementCalculatorService.filterByAverageDeviation

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -2,19 +2,51 @@ package com.aldisued.iot.monitoring.service;
 
 
 import java.util.List;
+import java.util.Objects;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MeasurementCalculatorService {
 
   public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
-    // TODO: Task 9
-    return List.of();
+    Objects.requireNonNull(values, "values must not be null");
+    Objects.requireNonNull(deviation, "deviation cannot be null");
+
+    validateFilterByAverageDeviationInput(values, deviation);
+
+    double average = values.stream()
+        .mapToDouble(Double::doubleValue)
+        .average()
+        .orElse(0.0);
+    double deviationValue = Math.abs(average) * deviation;
+    double lowerRange = average - deviationValue;
+    double upperRange = average + deviationValue;
+
+    return values.stream()
+        .filter(value -> doFilterByAverageDeviation(value, lowerRange, upperRange))
+        .toList();
   }
 
   public List<Double> getMovingAverage(List<Double> data, int windowSize) {
     // TODO: Task 10
     return List.of();
+  }
+
+  private void validateFilterByAverageDeviationInput(List<Double> values, double deviation) {
+    // Since no explicit requirement exists for null values, I decided to treat the input list as invalid, if it
+    // contains any null values. In a real scenario, missing requirements like this need to be clarified before the task
+    // is started.
+    if (values.stream().anyMatch(Objects::isNull)) {
+      throw new IllegalArgumentException("Data must not contain null values.");
+    }
+    if (!Double.isFinite(deviation) || deviation < 0.0 || deviation > 1.0) {
+      throw new IllegalArgumentException("Deviation must be between 0.0 and 1.0.");
+    }
+  }
+
+  private boolean doFilterByAverageDeviation(double value, double lowerRange, double upperRange) {
+    return value >= lowerRange && value <= upperRange;
   }
 
 }
--- src/test/java/com/aldisued/iot/monitoring/tasks/Task9Tests.java
+++ src/test/java/com/aldisued/iot/monitoring/tasks/Task9Tests.java
@@ -1,16 +1,27 @@
 package com.aldisued.iot.monitoring.tasks;
 
 import com.aldisued.iot.monitoring.service.MeasurementCalculatorService;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class Task9Tests {
+
+  private static final List<Double> LIST_WITH_NULL_VALUES = new ArrayList<>(3);
+
+  static {
+    LIST_WITH_NULL_VALUES.add(5.0);
+    LIST_WITH_NULL_VALUES.add(null);
+    LIST_WITH_NULL_VALUES.add(6.0);
+  }
 
   private final MeasurementCalculatorService measurementCalculatorService
       = new MeasurementCalculatorService();
@@ -25,6 +36,12 @@ public class Task9Tests {
         values, deviation);
 
     Assertions.assertEquals(expectedValues, filteredByAverageDeviation);
+  }
+
+  @Test
+  public void verifyFilteredAverageDeviationFailsOnValuesWithNull() {
+    Assertions.assertThrows(IllegalArgumentException.class,
+        () -> measurementCalculatorService.filterByAverageDeviation(LIST_WITH_NULL_VALUES, 0.2));
   }
 
   @ParameterizedTest
@@ -51,6 +68,11 @@ public class Task9Tests {
             List.of(0.0, 100.0),
             0.99,
             Collections.emptyList()
+        ),
+        Arguments.of(
+            List.of(-5.0, -15.0, -9.0, -11.0),
+            0.4,
+            List.of(-9.0, -11.0)
         ),
         Arguments.of(
             Collections.emptyList(),
