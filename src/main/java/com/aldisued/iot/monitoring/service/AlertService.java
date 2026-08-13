f07d4f842ff22bfd166cdbdb0bc4dcc82879659a
jtakacs <pti.jodzsi@gmail.com>
2026-08-13 16:10:04 +0200

    Task5: Created endpoint and service implementation for retrieving the latest alert for a sensor

--- src/main/java/com/aldisued/iot/monitoring/service/AlertService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/AlertService.java
@@ -2,8 +2,14 @@ package com.aldisued.iot.monitoring.service;
 
 import com.aldisued.iot.monitoring.dto.AlertDto;
 import com.aldisued.iot.monitoring.entity.Alert;
+import com.aldisued.iot.monitoring.entity.Sensor;
+import com.aldisued.iot.monitoring.exception.NoAlertsForSensorException;
+import com.aldisued.iot.monitoring.exception.SensorNotFoundException;
 import com.aldisued.iot.monitoring.repository.AlertRepository;
 import com.aldisued.iot.monitoring.repository.SensorRepository;
+
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -28,7 +34,18 @@ public class AlertService {
   }
 
   public AlertDto findLastAlertBySensorId(UUID sensorId) {
-    // TODO: Task 5
-    return null;
+    Objects.requireNonNull(sensorId, "sensorId cannot be null");
+
+    return alertRepository.findFirstBySensorIdOrderByTimestampDesc(sensorId)
+        .map(this::mapDTO)
+        .orElseThrow(NoAlertsForSensorException::new);
+  }
+
+  private AlertDto mapDTO(Alert alert) {
+    UUID sensorId = Optional.of(alert)
+        .map(Alert::getSensor)
+        .map(Sensor::getId)
+        .orElseThrow(SensorNotFoundException::new);
+    return new AlertDto(sensorId, alert.getMessage(), alert.getTimestamp());
   }
 }
